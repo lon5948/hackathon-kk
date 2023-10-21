f8cd73f3d1d3ba8bebcda95197c7534538744e0d
dasbd72 <twbd723@gmail.com>
2023-10-21 16:07:31 +0800

    added listener

--- backend/routers/chatroom.py
+++ backend/routers/chatroom.py
@@ -11,36 +11,46 @@ router = APIRouter()
 @router.websocket("/ws/chatroom/{client_id}")
 async def ws_symbol_events(websocket: WebSocket, client_id: str):
     await consoleManager.connect(websocket)
-    await consoleManager.broadcast_json(
-        {
-            "timestamp": int(time.time() * 1000),
-            "event-type": "system",
-            "client-id": client_id,
-            "nickname": "System",
-            "message": f"Client #{client_id} joined the chat",
-        }
-    )
-    try:
-        while True:
-            # receive client message or continue if timeout
-            try:
-                data = await asyncio.wait_for(
-                    websocket.receive_text(), timeout=1
-                )
-            except asyncio.TimeoutError:
-                continue
 
-            # send message to all clients
-            await consoleManager.broadcast(data)
-
-    except WebSocketDisconnect:
-        consoleManager.disconnect(websocket)
+    if client_id != "listener":
         await consoleManager.broadcast_json(
             {
                 "timestamp": int(time.time() * 1000),
                 "event-type": "system",
                 "client-id": client_id,
                 "nickname": "System",
-                "message": f"Client #{client_id} left the chat",
+                "message": f"Client #{client_id} joined the chat",
             }
         )
+        try:
+            while True:
+                # receive client message or continue if timeout
+                try:
+                    data = await asyncio.wait_for(
+                        websocket.receive_text(), timeout=1
+                    )
+                except asyncio.TimeoutError:
+                    continue
+
+                # send message to all clients
+                await consoleManager.broadcast(data)
+
+        except WebSocketDisconnect:
+            consoleManager.disconnect(websocket)
+            await consoleManager.broadcast_json(
+                {
+                    "timestamp": int(time.time() * 1000),
+                    "event-type": "system",
+                    "client-id": client_id,
+                    "nickname": "System",
+                    "message": f"Client #{client_id} left the chat",
+                }
+            )
+    else:
+        try:
+            while True:
+                # async sleep
+                await asyncio.sleep(1)
+
+        except WebSocketDisconnect:
+            consoleManager.disconnect(websocket)
