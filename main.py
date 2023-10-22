ed8497290fd6ec408c28d63180c95a2b085d04dc
c1uc <duck.cs09@nycu.edu.tw>
2023-10-22 10:31:54 +0800

    delete backend in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 from listener import run_listener
 from mailer import run_mailer
-from backend import run_backend
 import multiprocessing
 
 
@@ -17,17 +16,14 @@ def run_async_function(func):
 if __name__ == "__main__":
     process1 = multiprocessing.Process(target=run_async_function, args=(run_listener,))
     # process2 = multiprocessing.Process(target=run_async_function, args=(run_mailer,))
-    process3 = multiprocessing.Process(target=run_async_function, args=(run_backend,))
 
     # Start the processes
     process1.start()
     # process2.start()
-    process3.start()
 
     # Join the processes (this will block the main program until the child processes finish)
     process1.join()
     # process2.join()
-    process3.join()
 
 
 
