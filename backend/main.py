1b8b5dc8369d7344df7b10103cb1ef3e161d4940
dasbd72 <twbd723@gmail.com>
2023-10-22 09:53:48 +0800

    Change backend import

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.routers import chatroom, users
+from routers import chatroom, users
 
 load_dotenv()
 
